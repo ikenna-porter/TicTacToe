9b2b0d37915680a15950d73283df98d029b98670
ikenna <ikenna.porter@gmail.com>
2022-02-13 12:11:07 +0100

    Finished game logic

--- tictactoe.js
+++ tictactoe.js
@@ -16,19 +16,42 @@ function playerPlay () {
     return playerInput;
 }
 
-//Conditions to determine winner
+//Establishes conditions to determine winner and counts how many games have a winner
+let playerWins = 0;
+let computerWins = 0;
+
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerWins++;
         return `${playerSelection} beats ${computerSelection}. You win!`
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return `${playerSelection} beats ${computerSelection}. You win!`
+        playerWins++;
+        return `${playerSelection} beat ${computerSelection}. You win!`
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerWins++;
         return `${playerSelection} beats ${computerSelection}. You win!`
     } else if (playerSelection === computerSelection) {
         return "It's a tie. Play another round."
     } else {
+        computerWins++;
         return `${computerSelection} beats ${playerSelection}. You lose!`
     }
 }
 
-console.log(playRound(playerPlay(), computerPlay()));
+
+//Game with 5 rounds. Determines winner of 5 rounds.
+function game(round) {
+
+    while (playerWins + computerWins < 5) {
+        console.log(playRound(playerPlay(), computerPlay()));
+    }
+
+    if (playerWins > computerWins) {
+        console.log("Congrats! You've won the game!!!")
+    } else {
+        console.log("Uh oh...you've lost the game. Try again.")
+    }
+
+}
+
+game(playRound(playerPlay(), computerPlay()));
